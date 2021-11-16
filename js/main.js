c116b5d25ad0e1851290c5d3528ac536909214a2
Tommaso Passarotto <passarotto13@gmail.com>
2021-11-16 11:28:39 +0100

    tentivo di aggiungere classi corrette

--- js/main.js
+++ js/main.js
@@ -8,19 +8,19 @@ var chats = new Vue ({
                 visible: true,
                 messages: [
                     {
-                        date: ' 10/01/2020 15:30:55',
-                        text: ' Hai portato a spasso il cane?',
-                        status: ' sent'
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        status: 'sent'
                     },
                     {
-                        date: ' 10/01/2020 15:50:00',
-                        text: ' Ricordati di dargli da mangiare',
-                        status: ' sent'
+                        date: '10/01/2020 15:50:00',
+                        text: 'Ricordati di dargli da mangiare',
+                        status: 'sent'
                     },
                     {
-                        date: ' 10/01/2020 16:15:22',
-                        text: ' Tutto fatto!',
-                        status: ' received'
+                        date: '10/01/2020 16:15:22',
+                        text: 'Tutto fatto!',
+                        status: 'received'
                     }
                 ],
             },
@@ -30,19 +30,19 @@ var chats = new Vue ({
                 visible: true,
                 messages: [
                     {
-                        date: ' 20/03/2020 16:30:00',
-                        text: ' Ciao come stai?',
-                        status: ' sent'
+                        date: '20/03/2020 16:30:00',
+                        text: 'Ciao come stai?',
+                        status: 'sent'
                     },
                     {
-                        date: ' 20/03/2020 1 6:30:55',
-                        text: ' Bene grazie! Stasera ci vediamo?',
-                        status: ' received'
+                        date: '20/03/2020 1 6:30:55',
+                        text: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
                     },
                     {
-                        date: ' 20/03/2020 16:35:00',
-                        text: ' Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: ' sent'
+                        date: '20/03/2020 16:35:00',
+                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'sent'
                     }
                 ],
             },  
@@ -52,19 +52,19 @@ var chats = new Vue ({
                 visible: true,
                 messages: [
                     {
-                        date: ' 28/03/2020 10:10:40',
-                        text: ' La Marianna va in campagna',
-                        status: ' received'
+                        date: '28/03/2020 10:10:40',
+                        text: 'La Marianna va in campagna',
+                        status: 'received'
                     },
                     {
-                        date: ' 28/03/2020 10:20:10',
-                        text: ' Sicuro di non aver sbagliato chat?',
-                        status: ' sent'
+                        date: '28/03/2020 10:20:10',
+                        text: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
                     },
                     {
-                        date: ' 28/03/2020 16:15:22',
-                        text: ' Ah scusa!',
-                        status: ' received'
+                        date: '28/03/2020 16:15:22',
+                        text: 'Ah scusa!',
+                        status: 'received'
                     }
                 ],
             },
@@ -74,17 +74,22 @@ var chats = new Vue ({
                 visible: true,
                 messages: [
                     {
-                        date: ' 10/01/2020 15:30:55',
-                        text: ' Lo sai che ha aperto una nuova pizzeria?',
-                        status: ' sent'
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
                     },
                     {
-                        date: ' 10/01/2020 15:50:00',
-                        text: ' Si, ma preferirei andare al cinema',
-                        status: ' received'
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
                     }
                 ],
             },
-        ]
+        ],
+            methods: {
+                chatAttiva () {
+                    
+                }
+            }
     }
 });
